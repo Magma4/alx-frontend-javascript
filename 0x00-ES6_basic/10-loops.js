4f0bfefec1be732eb76212b9e4894da9fcafbe70
Magma <raymondamoateng@gmail.com>
2023-01-24 17:04:31 +0000

    Added the return code

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -3,4 +3,6 @@ export default function appendToEachArrayValue(array, appendString) {
     // eslint-disable-next-line no-param-reassign
     array[array.indexOf(value)] = appendString + value;
   }
+
+  return array;
 }
