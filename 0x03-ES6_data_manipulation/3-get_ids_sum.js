85cb0d0bc11c3044fea75d99bed3d6308508cf83
Magma <raymondamoateng@gmail.com>
2023-01-31 13:16:49 +0000

    reduction

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,3 @@
-const getStudentIdsSum = (students) => students.reduce((prevVal, cur) => prevVal + cur.id, 0);
-
-export default getStudentIdsSum;
+export default function getStudentIdsSum(students) {
+  return students.reduce((acc, c) => acc + c.id, 0);
+}
